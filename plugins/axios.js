9ff8eedbbd2872ac453b8416bb5655e8008cfbea
MrNaif2018 <chuff184@gmail.com>
2020-04-01 15:01:14 +0300

    Switch to OAuth tokens instead of JWT, fixes

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,56 +1,13 @@
 import axios from 'axios'
-import Cookies from 'js-cookie'
 
-function getCookie (cookieName, stringCookie) {
-  if (!stringCookie) { return undefined }
-  const strCookie = new RegExp('' + cookieName + '[^;]+').exec(stringCookie)
-  if (!strCookie) { return undefined }
-  return unescape(strCookie[0] ? strCookie[0].toString().replace(/^[^=]+./, '') : '')
-}
-
-export default ({ store, req }, inject) => {
+export default ({ store }, inject) => {
   const instance = axios.create({ baseURL: store.state.env.URL })
+  inject('axios', instance)
   instance.interceptors.request.use(
     (config) => {
-      if (config.headers.refreshing) { config.headers.refreshing = false } else if (req) {
-        config.headers.authorization = `Bearer ${getCookie('access_token', req.headers.cookie)}`
-      } else { config.headers.authorization = `Bearer ${Cookies.get('access_token')}` }
       config.baseURL = store.state.env.URL
       return config
     },
     err => Promise.reject(err)
   )
-  instance.interceptors.response.use((response) => {
-    return response
-  },
-  function (error) {
-    if (process.server) {
-      return new Promise((resolve, reject) => {
-        reject(error)
-      })
-    }
-    const originalRequest = error.config
-    if (error.response.status === 401 && originalRequest.url.endsWith('/token')) {
-      return new Promise((resolve, reject) => {
-        reject(error)
-      })
-    }
-    if (error.response.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true
-      return instance.post('/token',
-        {
-          'email': store.state.env.EMAIL,
-          'password': store.state.env.PASSWORD
-        }
-      )
-        .then((res) => {
-          if (res.status === 200) {
-            Cookies.set('access_token', res.data.access_token)
-            return instance(originalRequest)
-          }
-        })
-    }
-    return Promise.reject(error)
-  })
-  inject('axios', instance)
 }
